dddeef3f4fbc07340b9cc36104191bfdcad500e5
fcordillot <florian.cordillot@gmail.com>
2015-04-03 16:28:38 +0200

    Init project w/ Yeoman

---
+++ gulp/inject.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+
+var wiredep = require('wiredep').stream;
+
+module.exports = function(options) {
+  gulp.task('inject', ['scripts', 'styles'], function () {
+    var injectStyles = gulp.src([
+      options.tmp + '/serve/{app,components}/**/*.css',
+      '!' + options.tmp + '/serve/app/vendor.css'
+    ], { read: false });
+
+
+    var injectScripts = gulp.src([
+      options.src + '/{app,components}/**/*.js',
+      '!' + options.src + '/{app,components}/**/*.spec.js',
+      '!' + options.src + '/{app,components}/**/*.mock.js'
+    ])
+    .pipe($.angularFilesort()).on('error', options.errorHandler('AngularFilesort'));
+
+    var injectOptions = {
+      ignorePath: [options.src, options.tmp + '/serve'],
+      addRootSlash: false
+    };
+
+    var wiredepOptions = {
+      directory: 'bower_components',
+      exclude: [/jquery/]
+    };
+
+    return gulp.src(options.src + '/*.html')
+      .pipe($.inject(injectStyles, injectOptions))
+      .pipe($.inject(injectScripts, injectOptions))
+      .pipe(wiredep(wiredepOptions))
+      .pipe(gulp.dest(options.tmp + '/serve'));
+
+  });
+};
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('fifatournament')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
