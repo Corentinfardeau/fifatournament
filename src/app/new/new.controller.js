c66b89509fb866c0e0fbb6dacec6a070cdfab14b
fcordillot <florian.cordillot@gmail.com>
2015-04-14 16:18:24 +0200

    Update styles

--- src/app/new/new.controller.js
+++ src/app/new/new.controller.js
@@ -3,54 +3,53 @@
 angular.module('fifatournament')
 
 	.controller('NewCtrl', function ($scope) {
-    
-        
-        var maxPlayer = 7;
-        var minPlayer = 2;
-        $scope.players = [1,2]; 
+		
+		var maxPlayer = 7;
+		var minPlayer = 2;
+		$scope.players = [1,2]; 
 
-        $scope.incrementPlayer = function() {
-            
-            if ($scope.countPlayer >= maxPlayer) { return; }
-            
-            $scope.countPlayer++;
-            
-            $scope.players = []; 
-            for( var i = 0 ; i < $scope.countPlayer; i++){
-                $scope.players.push(i+1);
-            }
+		$scope.incrementPlayer = function() {
+			
+			if ($scope.countPlayer >= maxPlayer) { return; }
+			
+			$scope.countPlayer++;
+			
+			$scope.players = []; 
+			for( var i = 0 ; i < $scope.countPlayer; i++){
+				$scope.players.push(i+1);
+			}
 
-        };
+		};
     
-        $scope.decrementPlayer = function() {
-            if ($scope.countPlayer <= minPlayer) { return; }
-            $scope.countPlayer--;
-            
-            $scope.players = []; 
-            
-            for( var i = 0 ; i < $scope.countPlayer; i++){
-                $scope.players.push(i+1);
-            }
-        };
+		$scope.decrementPlayer = function() {
+			if ($scope.countPlayer <= minPlayer - 1) { return; }
+			$scope.countPlayer--;
+			
+			$scope.players = []; 
+			
+			for( var i = 0 ; i < $scope.countPlayer; i++){
+				$scope.players.push(i+1);
+			}
+		};
     
-        $scope.incrementPlayerByTeam = function() {
-            if ($scope.countPlayerByTeam >= $scope.countPlayer-1) { return; }
-            $scope.countPlayerByTeam++;
-        };
+		$scope.incrementPlayerByTeam = function() {
+			if ($scope.countPlayerByTeam >= $scope.countPlayer-1) { return; }
+			$scope.countPlayerByTeam++;
+		};
     
-        $scope.decrementPlayerByTeam = function() {
-            if ($scope.countPlayerByTeam <= minPlayer) { return; }
-            $scope.countPlayerByTeam--;
-        };
+		$scope.decrementPlayerByTeam = function() {
+			if ($scope.countPlayerByTeam <= minPlayer) { return; }
+			$scope.countPlayerByTeam--;
+		};
     
 		$scope.createInput = function($event) {
-            
-            
-        }
+			
+			
+		}
     
-        $scope.create = function(){
-            alert("Crée");
-        };
+		$scope.create = function(){
+			alert("Crée");
+		};
 
 	});
 
