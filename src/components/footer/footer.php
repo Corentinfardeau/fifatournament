5985d9fb3b8a68f4ba7cd903299d8dcb1556e74c
fcordillot <florian.cordillot@gmail.com>
2015-04-14 20:56:51 +0200

    Update class & styles

--- src/components/footer/footer.php
+++ src/components/footer/footer.php
@@ -1,4 +1,4 @@
-<footer class="footer <?php if($_GET["b"] == "bottom"): echo 'bottom'; endif; ?> <?php if($_GET["bg"] === "black"): echo 'white'; else: echo "black"; endif; ?>">
+<footer class="footer">
 	&copy; 2015 - Made by <a href="https://twitter.com/corentinfardeau" alt="@corentinfardeau" title="@corentinfardeau">@corentinfardeau</a> & <a href="https://twitter.com/fcordillot" alt="@fcordillot" title="@fcordillot">@fcordillot</a> with <span class="heart">&hearts;</span>
 	<p>
 		Fifatournament V1.0
--- src/components/navbar/navbar.php
+++ src/components/navbar/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar <?php if($_GET["bg"] && $_GET["bg"] === "black"): echo 'white'; else: echo "black"; endif; ?>" ng-controller="NavbarCtrl">
+<nav class="navbar" ng-controller="NavbarCtrl">
 	<ul>
 		<li><a ng-href="#">Qui sommes-nous ?</a></li>
 		<li><a ng-href="#" class="twitter">Twitter</a></li>
