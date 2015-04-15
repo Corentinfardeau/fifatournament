c768519f78c764fecc52b6f226892bf15fbb7a6e
fcordillot <florian.cordillot@gmail.com>
2015-04-15 19:41:44 +0200

    Add match card

--- src/app/matchs/matchs.controller.js
+++ src/app/matchs/matchs.controller.js
@@ -2,7 +2,21 @@
 
 angular.module('fifatournament')
 	.controller('MatchsCtrl', function ($scope) {
+		var state = 0;
+		var translateX = (window.innerWidth / 2) - 296;
+		var wrapper = document.getElementById('matchs-wrapper');
 
+		$scope.translateR = function() {
+			state++;
+			translateX -= 499.5;
+			wrapper.style.webkitTransform = 'translate3D(' + translateX + 'px,0,0)';
+		}
+		$scope.translateL = function() {
+			if(state === 0) return;
+			state--;
+			translateX += 499.5;
+			wrapper.style.webkitTransform = 'translate3D(' + translateX + 'px,0,0)';
+		}
 	})
 	.directive('macths',function() {
 		return  {
