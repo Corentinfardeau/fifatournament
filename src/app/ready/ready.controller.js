1a6a7bbfa32b75bea3e9ff34d2151d5421024ffa
Corentin FARDEAU <corentinfardeau@aol.fr>
2015-04-15 17:09:35 +0200

    - Disable input - hide mix button

--- src/app/ready/ready.controller.js
+++ src/app/ready/ready.controller.js
@@ -15,27 +15,27 @@ angular.module('fifatournament')
         //Create random team with all attributs
         $scope.alea = function(colors) {
             
-            var config = JSON.parse(localStorage.getItem('configTournois'));
-            var nb_teams_complete = (config.nb_players - (config.nb_players % config.nb_players_by_team))/config.nb_players_by_team;
-            var nb_players_last_team = config.nb_players - (config.nb_players_by_team*nb_teams_complete);
+            $scope.config = JSON.parse(localStorage.getItem('configTournois'));
+            var nb_teams_complete = ($scope.config.nb_players - ($scope.config.nb_players % $scope.config.nb_players_by_team))/$scope.config.nb_players_by_team;
+            var nb_players_last_team = $scope.config.nb_players - ($scope.config.nb_players_by_team*nb_teams_complete);
             var players_last_team = [];
                 
             $scope.teams = [];
             
-            var players_name_shuffle = $scope.shuffle(config.players_name);
+            var players_name_shuffle = $scope.shuffle($scope.config.players_name);
             
             //Create full team
             for (var i = 0 ; i < nb_teams_complete ; i++){
                 
                 var players_name = [];
                 
-                for(var k =0; k < config.nb_players_by_team; k++){
+                for(var k =0; k < $scope.config.nb_players_by_team; k++){
                     players_name.push(players_name_shuffle[players_name_shuffle.length-1]);    
                     players_name_shuffle.pop();
                 }
                 
                 var team = {
-                    "nb_player" : config.nb_players_by_team,
+                    "nb_player" : $scope.config.nb_players_by_team,
                     "name" : "Nom d'équipe "+(i+1),
                     "couleur" : colors[i],
                     "players_name" : players_name
@@ -53,7 +53,7 @@ angular.module('fifatournament')
                     players_last_team.push('');
                 }
 
-                if(config.alea){
+                if($scope.config.alea){
                     var team = {
                         "nb_players" : nb_players_last_team,
                         "name" : "Nom d'équipe "+($scope.teams.length+1),
@@ -130,7 +130,11 @@ angular.module('fifatournament')
             restrict: 'E',
             template : '<div class="card-edit">' + 
             '<div class="header" style="background-color: {{team.couleur}}"><input type="text" class="team_name_input" placeholder="{{team.name}}"/><i class="fa fa-fw fa-pencil" ng-click="clickEdit()"></i></div>' +
-            '<div class="content"><input placeholder="Saisissez un nom" ng-repeat="player in team.players_name track by $index" value="{{player}}"></div>' +
-            '</div>'
+            '<div class="content"><input ng-disabled="config.alea" placeholder="Saisissez un nom" ng-repeat="player in team.players_name track by $index" value="{{player}}"></div>' +
+            '</div>',
+            
+            link: function ($scope, $element) {
+                console.log($scope.config.alea);
+            }
         }
     });
