d4cb76203df89c03c0df96c8343c1b4ebdfe0228
fcordillot <florian.cordillot@gmail.com>
2015-04-16 18:18:56 +0200

    Add table for results

--- src/app/matchs/matchs.controller.js
+++ src/app/matchs/matchs.controller.js
@@ -6,10 +6,18 @@ angular.module('fifatournament')
 		var translateX = (window.innerWidth / 2) - 296;
 		var wrapper = document.getElementById('matchs-wrapper');
 		var start_btn = document.getElementById('start-btn');
+		var stop_btn = document.getElementById('stop-btn');
 		var matchs = document.getElementsByClassName('match-card');
-		var matchs_active = document.querySelectorAll('.match-card.active');
+		var live = false;
 
 		$scope.league = JSON.parse(localStorage.getItem('league'));
+
+		$scope.calcMatchs = function() {
+			$scope.nbMatchsAller = 0;
+			$scope.nbMatchsRetour = 0;
+			for(var i = 0, l = $scope.league.aller.length; i < l; i++) {
+			}
+		}
 
 		$scope.start_match = function() {
 			for(var i = 0, l = matchs.length; i < l; i++) {
@@ -19,18 +27,28 @@ angular.module('fifatournament')
 			matchs[state].classList.remove('disabled');
 			matchs[state].classList.add('active');
 			document.getElementById('title').classList.add('active');
-			start_btn.innerHTML = "Fin du match";
-			matchs_active = document.querySelectorAll('.match-card.active');
+			document.getElementById('btn-wrapper').classList.add('active');
+			live = true;
+		}
+
+		$scope.stop_match = function() {
+			for(var i = 0, l = matchs.length; i < l; i++) {
+				matchs[i].classList.remove('disabled');
+				matchs[i].classList.remove('active');
+			}
+			document.getElementById('title').classList.remove('active');
+			document.getElementById('btn-wrapper').classList.remove('active');
+			live = false;
 		}
 
 		$scope.translateR = function() {
-			if(state === $scope.league.aller.length - 1 || matchs_active.length > 0) return;
+			if(state === $scope.league.aller.length - 1 || live) return;
 			state++;
 			translateX -= 499.5;
 			wrapper.style.webkitTransform = 'translate3D(' + translateX + 'px,0,0)';
 		}
 		$scope.translateL = function() {
-			if(state === 0 || matchs_active.length > 0) return;
+			if(state === 0 || live) return;
 			state--;
 			translateX += 499.5;
 			wrapper.style.webkitTransform = 'translate3D(' + translateX + 'px,0,0)';
@@ -41,17 +59,17 @@ angular.module('fifatournament')
 			restrict: 'E',
 			template : '<div class="match-card">' +
 				'<div class="home">' +
-					'<span class="score" style="color: {{match[0].couleur}};">0</span>' +
+					'<span class="score" style="color: {{match[0].couleur}};">{{score_team_1}}</span>' +
 					'<span class="name">{{match[0].name}}</span>' +
-					'<span class="btn" style="background-color: {{match[0].couleur}};">But</span>' +
+					'<span class="btn" ng-init="score_team_1 = 0" ng-click="score_team_1 = score_team_1 + 1" style="background-color: {{match[0].couleur}};">But</span>' +
 				'</div>' +
 				'<div class="center">' +
 					'-' +
 				'</div>' +
 				'<div class="outdoor">' +
-					'<span class="score" style="color: {{match[1].couleur}};">0</span>' +
+					'<span class="score" style="color: {{match[1].couleur}};">{{score_team_2}}</span>' +
 					'<span class="name">{{match[1].name}}</span>' +
-					'<span class="btn" style="background-color: {{match[1].couleur}};">But</span>' +
+					'<span class="btn" ng-init="score_team_2 = 0" ng-click="score_team_2 = score_team_2 + 1" style="background-color: {{match[1].couleur}};">But</span>' +
 				'</div>' +
 			'</div>',
 			link: function ($scope, $element) {
