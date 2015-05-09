cada04283b9f98df896a2a11907f805b7eb81c7e
Corentin FARDEAU <corentinfardeau@aol.fr>
2015-05-09 14:41:31 +0200

    - API updated for players and teams

--- API/app/models/player.js
+++ API/app/models/player.js
@@ -5,9 +5,11 @@ var playerSchema = mongoose.Schema({
     
     'playerName' : String,
     'nbGoal' : {type:Number, default:0},
-    'team' : mongoose.Schema.ObjectId
+    'team' : mongoose.Schema.ObjectId,
+    'tournament' : mongoose.Schema.ObjectId
 });
 
 playerSchema.belongsTo('Team', {through: 'team'});
+playerSchema.belongsTo('Tournament', {through: 'tournament'});
 
 module.exports = mongoose.model('Player', playerSchema);
