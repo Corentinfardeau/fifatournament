5ec4a3ef2cf1a458f2cf6f542cd9ba28784c8b92
Florian Cordillot <florian.cordillot@gmail.com>
2015-05-21 01:47:49 +0200

    Updated styles

--- desktop/src/components/footer/footer.php
+++ desktop/src/components/footer/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer" ng-controller="FooterCtrl">
 	&copy; 2015 - Made by <a href="https://twitter.com/corentinfardeau" alt="@corentinfardeau" title="@corentinfardeau">@corentinfardeau</a> & <a href="https://twitter.com/fcordillot" alt="@fcordillot" title="@fcordillot">@fcordillot</a> with <span class="heart">&hearts;</span>
 	<p>
-		Fifatournament <a href="#/changelog" class="red">V1.0</a> &nbsp;- &nbsp;<a href="#/credits" class="red-inverse">Crédits</a>
+		Fifatournament <a href="#/changelog">V1.0</a> &nbsp;- &nbsp;<a href="#/credits">Crédits</a>
 	</p>
 </footer>
