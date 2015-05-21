bcff3ebf0a5d8a8e7a39bf2379bfa324a77280a9
Corentin FARDEAU <corentinfardeau@aol.fr>
2015-05-21 17:34:01 +0200

    - randomController updated

--- desktop/src/app/scripts/controllers/random.controller.js
+++ desktop/src/app/scripts/controllers/random.controller.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('fifatournament')
-  .controller('RandomCtrl', function ($scope, LocalStorage, JSON, displayMessages, API, $location) {
+  .controller('RandomCtrl', function ($scope, LocalStorage, API) {
+    
+    $scope.init = function(){
+        
+        API.getTournament(LocalStorage.getLocalStorage('tournament'))
+        .success(function(tournament){
+            
+            console.log(tournament);
+            $scope.players = [];
+            
+            for(var j = 0; j < tournament.nbPlayers; j++){
+                $scope.players.push("");
+            }
+            
+            console.log($scope.players);
+        })
+        .error(function(err){
+            console.log(err);
+        })
+        
+    };
+    
+    $scope.init();
     
 });
