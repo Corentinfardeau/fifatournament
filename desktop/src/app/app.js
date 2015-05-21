82ffc37df684cf151a068cb523fa547c6a3243ac
Florian Cordillot <florian.cordillot@gmail.com>
2015-05-21 17:34:29 +0200

    Change name + edit styles

--- desktop/src/app/app.js
+++ desktop/src/app/app.js
@@ -4,57 +4,57 @@ angular.module('fifatournament', ['ngRoute'])
 	.config(function ($routeProvider) {		
 		$routeProvider
 			.when('/', {
-				title: 'Accueil | FifaTournament',
+				title: 'Accueil | Soccup',
 				templateUrl: 'app/views/main.html',
                 controller: 'MainCtrl'
 			})
 			.when('/config', {
-				title: 'Configuration | FifaTournament',
+				title: 'Configuration | Soccup',
 				templateUrl: 'app/views/config.html',
 				controller: 'ConfigCtrl'
 			})
 			.when('/random', {
-				title: 'Random | FifaTournament',
+				title: 'Random | Soccup',
 				templateUrl: 'app/views/random.html',
 				controller: 'RandomCtrl'
 			})
 			.when('/ready', {
-				title: 'Prêt ? | FifaTournament',
+				title: 'Prêt ? | Soccup',
 				templateUrl: 'app/views/ready.html',
 				controller: 'ReadyCtrl'
 			})
 			.when('/matchs', {
-				title: 'Prochain(s) match(s) | FifaTournament',
+				title: 'Prochain(s) match(s) | Soccup',
 				templateUrl: 'app/views/matchs.html',
 				controller: 'MatchsCtrl'
 			})
             .when('/join', {
-				title: 'Prochain(s) match(s) | FifaTournament',
+				title: 'Prochain(s) match(s) | Soccup',
 				templateUrl: 'app/views/join.html',
 				controller: 'JoinController'
 			})
 			.when('/latest_matchs', {
-				title: 'Matchs passés | FifaTournament',
+				title: 'Matchs passés | Soccup',
 				templateUrl: 'app/views/latest_matchs.html',
 				controller: 'LatestMatchsCtrl'
 			})
 			.when('/results', {
-				title: 'Résultats | FifaTournament',
+				title: 'Résultats | Soccup',
 				templateUrl: 'app/views/results.html',
 				controller: 'ResultsCtrl'
 			})
 			.when('/end', {
-				title: 'Victoire | FifaTournament',
+				title: 'Victoire | Soccup',
 				templateUrl: 'app/views/end.html',
 				controller: 'EndCtrl'
 			})
 			.when('/changelog', {
-				title: 'Changelog | FifaTournament',
+				title: 'Changelog | Soccup',
 				templateUrl: 'app/views/changelog.html',
 				controller: 'ChangelogCtrl'
 			})
 			.when('/credits', {
-				title: 'Crédits | FifaTournament',
+				title: 'Crédits | Soccup',
 				templateUrl: 'app/views/credits.html',
 				controller: 'CreditsCtrl'
 			})
