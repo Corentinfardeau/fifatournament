f48e7b8743286c87867b980b8a39a91ab423d857
Valentin Soyer <valent.soyer@gmail.com>
2015-05-21 19:44:47 +0200

    fist page begin

--- android/Soccup/app/src/main/java/com/soccup/ConfigurationActivity.java
+++ android/Soccup/app/src/main/java/com/soccup/ConfigurationActivity.java
@@ -1,35 +1,24 @@
 package com.soccup;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class ConfigurationActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button beginButton = (Button)findViewById(R.id.begin);
-        beginButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ConfigurationActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_configuration);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_configuration, menu);
         return true;
     }
 
