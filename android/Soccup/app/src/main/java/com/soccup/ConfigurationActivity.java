c6eaf3aa027f62ae8c7f6ea4a5d663f0716692ba
Valentin Soyer <valent.soyer@gmail.com>
2015-05-22 12:50:38 +0200

    update

--- android/Soccup/app/src/main/java/com/soccup/ConfigurationActivity.java
+++ android/Soccup/app/src/main/java/com/soccup/ConfigurationActivity.java
@@ -1,12 +1,12 @@
 package com.soccup;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class ConfigurationActivity extends ActionBarActivity {
+public class ConfigurationActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
