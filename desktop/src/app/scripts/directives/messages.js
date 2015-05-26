ccfb1a139f5a93b67dd6d3bcac76b1f7809571f8
Corentin FARDEAU <corentinfardeau@aol.fr>
2015-05-26 16:45:20 +0200

    - error message updated

--- desktop/src/app/scripts/directives/messages.js
+++ desktop/src/app/scripts/directives/messages.js
@@ -5,9 +5,6 @@ angular.module('fifatournament')
     return {
       templateUrl: 'app/views/partials/_messages.html',
       restrict: 'E',
-      replace: true,
-      link: function(scope,elem,attr) {
-        elem[0].innerHTML = attr.inner;
-      }
+      replace: true
     };
 });
