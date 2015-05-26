5c782de0ca8323717a013200430c8a5f69ebef6e
maximedaguet <maxime.daguet@gmail.com>
2015-05-26 16:17:05 +0200

    Style updates (header)

--- desktop/src/components/navbar/navbar.php
+++ desktop/src/components/navbar/navbar.php
@@ -1,5 +1,6 @@
 <nav class="top-links">
 	<ul>
+		<li><a href="/#/new" class="links">Recommencer le tournoi<i class="fa fa-fw fa-refresh right"></i></a></li>
 		<li><a target="_blank" ng-href="https://www.facebook.com/soccup" class="facebook"><i class="icon i-facebook"></i></a></li>
 		<li><a target="_blank" ng-href="https://twitter.com/SoccupApp" class="twitter"><i class="icon i-twitter"></i></a></li>
 	</ul>
