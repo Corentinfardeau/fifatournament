b522b1c25c35e2036a72516aa07d285286f3632b
Valentin Soyer <valent.soyer@gmail.com>
2015-05-28 19:55:41 +0200

    Works on API

--- android/Soccup/app/src/main/java/com/soccup/Api.java
+++ android/Soccup/app/src/main/java/com/soccup/Api.java
@@ -1,5 +1,9 @@
 package com.soccup;
 
+import android.util.Log;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
@@ -11,18 +15,27 @@ import java.io.IOException;
  */
 public class Api {
     private OkHttpClient client = new OkHttpClient();
+    private Response response;
+    private Request request;
+    private Call call;
 
-    public void Api(){
-        //String response = this.run("https://raw.github.com/square/okhttp/master/README.md");
-        //System.out.println(response);
-    }
+    public Api(){
+        this.request = new Request.Builder().url("http://www.critikcine.fr/api/v1/films/").build();
+        this.call = this.client.newCall(this.request);
+        this.call.enqueue(new Callback() {
+            public void onFailure(Request request, IOException e) {
 
-    public String run(String url) throws IOException{
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
+            }
 
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+            public void onResponse(Response response) throws IOException {
+                try {
+                    if(response.isSuccessful()){
+                        Log.d("okHttp", response.body().string());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
