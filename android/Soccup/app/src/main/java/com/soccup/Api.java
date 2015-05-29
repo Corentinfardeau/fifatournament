1833c9c6f69095f290d1be89f20d169f72c0b241
Valentin Soyer <valent.soyer@gmail.com>
2015-05-29 16:05:49 +0200

    api test

--- android/Soccup/app/src/main/java/com/soccup/Api.java
+++ android/Soccup/app/src/main/java/com/soccup/Api.java
@@ -4,8 +4,10 @@ import android.util.Log;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
@@ -15,7 +17,7 @@ import java.io.IOException;
  */
 
 public class Api {
-
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private OkHttpClient client;
     private Response response;
     private Request request;
@@ -27,15 +29,26 @@ public class Api {
     }
 
     // CREATE
-    public void createTournament(String url) {
-        this.request = new Request.Builder().url(url).build();
-        this.call = this.client.newCall(this.request);
-        this.call.enqueue(new Callback(){
+    public void createTournament(String type, Boolean bePublic, Boolean alea, int nbPlayers, int nbPlayersByTeam){
+        String url = "http://localhost:8080/api/tournament/create";
+        String json = "{'type':'"+ type + "',"
+            + "'public':"+ bePublic + ","
+            + "'alea':"+ alea + ","
+            + "'nbPlayers':"+ nbPlayers + ","
+            + "'nbPlayersByTeam':"+ nbPlayersByTeam + "}";
+        Log.d("JSon", json);
 
+        RequestBody body = RequestBody.create(JSON, json);
+        this.request = new Request.Builder().url(url).post(body).build();
+        this.call = this.client.newCall(this.request);
+
+        this.call.enqueue(new Callback(){
+            // ON ERROR
             public void onFailure(Request request, IOException e) {
-                Log.d("erreur", "erreur lors de la création du tournament");
+                Log.d("erreur", "Erreur lors de la création du tournament");
             }
 
+            // ON SUCCESS
             public void onResponse(Response response) throws IOException {
                 try {
                     if (response.isSuccessful()) {
--- android/Soccup/app/src/main/java/com/soccup/MainActivity.java
+++ android/Soccup/app/src/main/java/com/soccup/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
         });
 
         Api api = new Api();
-        api.createTournament("http://www.critikcine.fr/api/v1/films/");
+        api.createTournament("league", true, true, 2, 1);
     }
 
 
