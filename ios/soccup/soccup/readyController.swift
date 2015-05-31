5bb1589956f2fd5e7daaed7639b0d3d94ed8c2c9
Corentin FARDEAU <corentinfardeau@aol.fr>
2015-05-31 16:39:09 +0200

    - add screen and navigation

--- ios/soccup/soccup/readyController.swift
+++ ios/soccup/soccup/readyController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  readyController.swift
 //  soccup
 //
-//  Created by Corentin FARDEAU on 29/05/2015.
+//  Created by Corentin FARDEAU on 31/05/2015.
 //  Copyright (c) 2015 soccup. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class readyController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
