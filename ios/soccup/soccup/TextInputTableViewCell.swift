ef2335d59f54a708ca3cab3f8300979995223f61
maximedaguet <maxime.daguet@gmail.com>
2015-06-05 16:44:45 +0200

    TableView for teams added

---
+++ ios/soccup/soccup/TextInputTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextInputTableViewCell.swift
+//  soccup
+//
+//  Created by Maxime DAGUET on 05/06/2015.
+//  Copyright (c) 2015 soccup. All rights reserved.
+//
+
+import UIKit
+
+public class TextInputTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    public func configure(#text: String?, placeholder: String) {
+        textField.text = text
+        textField.placeholder = placeholder
+        
+        textField.accessibilityValue = text
+        textField.accessibilityLabel = placeholder
+    }
+}
+
