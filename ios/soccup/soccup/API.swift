242ccb11e920c448d5e7567b6db0e1934cb9ee49
Corentin FARDEAU <corentinfardeau@aol.fr>
2015-06-05 12:20:20 +0200

    - LocalStorage added

--- ios/soccup/soccup/API.swift
+++ ios/soccup/soccup/API.swift
@@ -86,7 +86,6 @@ class API {
         
         Alamofire.request(.POST, self.apiURL+section, parameters: params)
             .responseJSON { request, response, responseObject, error in
-                println(responseObject)
                 completionHandler(responseObject: responseObject as! NSArray, error: error)
         }
     }
--- ios/soccup/soccup/readyController.swift
+++ ios/soccup/soccup/readyController.swift
@@ -14,11 +14,27 @@ class readyController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getTournament()
     }
+    
+    let localStorage = NSUserDefaults.standardUserDefaults()
+    let api = API()
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func getTournament(){
+        if let ID = self.localStorage.stringForKey("tournament"){
+            api.getTournament(ID, completionHandler:{
+                tournament, error in
+                if((error) != nil){
+                    println(error)
+                }else{
+                    println(tournament)
+                }
+            })
+        }
+    }
 }
