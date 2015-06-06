94bda3e09aea8d3ef434e8d9d217182e6740af1c
maximedaguet <maxime.daguet@gmail.com>
2015-06-06 13:38:19 +0200

    Live Match controller and storyboard updated

--- ios/soccup/soccup/liveMatchController.swift
+++ ios/soccup/soccup/liveMatchController.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class LiveMatchController: UIViewController {
 
+    @IBOutlet weak var labelScoreHomeTeam: UILabel!
+    @IBOutlet weak var labelScoreAwayTeam: UILabel!
+    
+    @IBOutlet weak var labelNameHomeTeam: UILabel!
+    @IBOutlet weak var labelNameAwayTeam: UILabel!
+    
+    @IBOutlet weak var buttonGoalHomeTeam: UIButton!
+    @IBOutlet weak var buttonGoalAwayTeam: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        labelScoreHomeTeam.text = "1"
+        labelScoreAwayTeam.text = "2"
+        
+        labelNameHomeTeam.text = "FC Barcelone"
+        labelNameAwayTeam.text = "Real Madrid"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
 }
