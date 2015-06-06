c69e17c61b06b775714d9c1cfbeba60a85f65825
maximedaguet <maxime.daguet@gmail.com>
2015-06-06 16:46:29 +0200

    Ranking table updated

--- ios/soccup/soccup/rankingMatchController.swift
+++ ios/soccup/soccup/rankingMatchController.swift
@@ -8,6 +8,30 @@
 
 import UIKit
 
-class RankingMatchController: UITableViewController, UITableViewDataSource, UITableViewDelegate  {
+class RankingMatchController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let teams = ["Equipe 1", "Equipe 2", "Equipe 3"]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return teams.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("rankingCell") as! RankingTableViewCell
+        cell.configure(nameTeam: "\(teams[indexPath.row])")
+        return cell
+    }
     
 }
+
+
