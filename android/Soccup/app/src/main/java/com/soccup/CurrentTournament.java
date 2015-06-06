1a0dfdd25f5b95cb5b410dc670c02b23d68a065d
Damien Laurent <damlau4@gmail.com>
2015-06-06 18:35:49 +0200

    create popup

--- android/Soccup/app/src/main/java/com/soccup/CurrentTournament.java
+++ android/Soccup/app/src/main/java/com/soccup/CurrentTournament.java
@@ -1,19 +1,104 @@
 package com.soccup;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 
 public class CurrentTournament extends Activity {
+    //Origin popup
+    Point p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.current_tournament);
+
+        //change view onglet
+        Button
+
+        //Button "add but team"
+        Button btnAddButHome = (Button)findViewById(R.id.addButTeamHome);
+        Button btnAddButAway = (Button)findViewById(R.id.addButTeamAway);
+
+        //Click team Home
+        btnAddButHome.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //Open popup window
+                if (p != null)
+                    showPopup(CurrentTournament.this, p);
+            }
+        });
+
+        //Click team away
+        btnAddButAway.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //Open popup window
+                if (p != null)
+                    showPopup(CurrentTournament.this, p);
+            }
+        });
     }
 
+    //Initialize origin popup
+    public void onWindowFocusChanged(boolean hasFocus) {
+
+        int[] location = new int[2];
+        LinearLayout box = (LinearLayout) findViewById(R.id.currentMatch);
+
+        // Get the x, y location and store it in the location[] array
+        // location[0] = x, location[1] = y.
+        box.getLocationOnScreen(location);
+
+        //Initialize the Point with x, and y positions
+        p = new Point();
+        p.x = location[0];
+        p.y = location[1];
+    }
+
+    // The method that displays the popup.
+    private void showPopup(final Activity context, Point p) {
+
+        // Inflate the popup_layout.xml
+        LinearLayout viewGroup = (LinearLayout) context.findViewById(R.id.popupAddScorer);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.add_scorer, viewGroup);
+
+        // Creating the PopupWindow
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(900);
+        popup.setHeight(800);
+        popup.setFocusable(true);
+
+        // Some offset to align the popup a bit to the right, and a bit down, relative to button's position.
+        int OFFSET_X = 20;
+        int OFFSET_Y = 20;
+
+        // Displaying the popup at the specified location, + offsets.
+        popup.showAtLocation(layout, Gravity.NO_GRAVITY, p.x + OFFSET_X, p.y + OFFSET_Y);
+
+        // Getting a reference to Close button, and close the popup when clicked.
+        Button close = (Button) layout.findViewById(R.id.cancel_scorer);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test", "test");
+                popup.dismiss();
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
