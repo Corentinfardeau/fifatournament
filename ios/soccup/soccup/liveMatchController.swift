e19fedf1b643d22a6f83fdecadc77bfecef6dfd5
maximedaguet <maxime.daguet@gmail.com>
2015-06-06 13:19:44 +0200

    List of matchs added

---
+++ ios/soccup/soccup/liveMatchController.swift
@@ -0,0 +1,13 @@
+//
+//  liveMatchController.swift
+//  soccup
+//
+//  Created by Maxime DAGUET on 06/06/2015.
+//  Copyright (c) 2015 soccup. All rights reserved.
+//
+
+import UIKit
+
+class LiveMatchController: UIViewController {
+
+}
