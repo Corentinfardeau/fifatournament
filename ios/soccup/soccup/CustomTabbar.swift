ffad9d50732d467b1f9d7d3288f0d2cb0acfb934
maximedaguet <maxime.daguet@gmail.com>
2015-06-10 11:01:11 +0200

    Custom tabbar

--- ios/soccup/soccup/CustomTabbar.swift
+++ ios/soccup/soccup/CustomTabbar.swift
@@ -6,7 +6,9 @@
 //
 //
 
+import Foundation
 import UIKit
+import CoreImage
 
 class CustomTabbar: UITabBarController {
 
@@ -16,6 +18,35 @@ class CustomTabbar: UITabBarController {
         tabBar.backgroundColor = UIColor.whiteColor()
         tabBar.shadowImage = UIImage()
         UITabBarItem.appearance().setTitleTextAttributes([NSFontAttributeName: UIFont(name: "SourceSansPro-regular", size: 12)!], forState: UIControlState.Normal)
+//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.magentaColor()], forState:.Normal)
+//        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.redColor()], forState:.Selected)
+//        
+//        for item in self.tabBar.items as! [UITabBarItem] {
+//            if let image = item.image {
+//                item.image = image.imageWithColor(UIColor.yellowColor()).imageWithRenderingMode(.AlwaysOriginal)
+//            }
+//        }
+//        
+//        extension UIImage {
+//            func imageWithColor(tintColor: UIColor) -> UIImage {
+//                UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+//                
+//                let context = UIGraphicsGetCurrentContext() as CGContextRef
+//                CGContextTranslateCTM(context, 0, self.size.height)
+//                CGContextScaleCTM(context, 1.0, -1.0);
+//                CGContextSetBlendMode(context, kCGBlendModeNormal)
+//                
+//                let rect = CGRectMake(0, 0, self.size.width, self.size.height) as CGRect
+//                CGContextClipToMask(context, rect, self.CGImage)
+//                tintColor.setFill()
+//                CGContextFillRect(context, rect)
+//                
+//                let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+//                UIGraphicsEndImageContext()
+//                
+//                return newImage
+//            }
+//        }
 
     }
 
