84da00cc130670fc91e29f0d14661adf7eb923ad
maximedaguet <maxime.daguet@gmail.com>
2015-06-11 17:03:57 +0200

    Tabbar updated

---
+++ ios/soccup/soccup/RemoveShadow.swift
@@ -0,0 +1,21 @@
+//
+//  RemoveShadow.swift
+//  soccup
+//
+//  Created by Maxime DAGUET on 11/06/2015.
+//  Copyright (c) 2015 soccup. All rights reserved.
+//
+
+import UIKit
+
+class RemoveShadow: UINavigationBar {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ios/soccup/soccup/TabbarIconColor.swift
@@ -0,0 +1,13 @@
+//
+//  TabbarIconColor.swift
+//  soccup
+//
+//  Created by Maxime DAGUET on 11/06/2015.
+//  Copyright (c) 2015 soccup. All rights reserved.
+//
+
+import UIKit
+
+class TabbarIconColor: UIImage {
+   
+}
