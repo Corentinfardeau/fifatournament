0c266541ecee509985cf11c6e5c43927c929a1f6
maximedaguet <maxime.daguet@gmail.com>
2015-06-11 16:07:22 +0200

    Colors and icons updates

---
+++ ios/soccup/soccup/Palette.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let mainColor = UIColor(red: 16.0/255.0, green: 210.0/255.0, blue: 99.0/255.0, alpha: 1.0)
+
+let secondaryColor = UIColor(red: 34.0/255.0, green: 31.0/255.0, blue: 95.0/255.0, alpha: 1.0)
+
+let darkColor = UIColor(red: 171.0/255.0, green: 174.0/255.0, blue: 205.0/255.0, alpha: 1.0)
+
+let lightColor = UIColor(red: 66.0/255.0, green: 74.0/255.0, blue: 247.0/255.0, alpha: 1.0)
