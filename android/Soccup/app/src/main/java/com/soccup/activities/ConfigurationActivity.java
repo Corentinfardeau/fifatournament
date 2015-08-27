34d5ab684fc4f033a13eed78fa311db018315640
Valentin Soyer <valent.soyer@gmail.com>
2015-08-27 21:42:51 +0200

    Revert "test"
    This reverts commit 05fb6c677b6214a2e1ce657af4568881051ba658.

--- android/Soccup/app/src/main/java/com/soccup/activities/ConfigurationActivity.java
+++ android/Soccup/app/src/main/java/com/soccup/activities/ConfigurationActivity.java
@@ -22,6 +22,7 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class ConfigurationActivity extends AppCompatActivity {
     private Boolean randomTeam = false;
     private String tournament;
